bc367d0ad294b57ba0bec6d3d2de4553f8e72aa7
dasushan <53913896+dasushan@users.noreply.github.com>
2024-09-05 13:12:01 +0530

    doing network calls and handling response the right way

--- src/store/ui-slice.js
+++ src/store/ui-slice.js
@@ -1,20 +1,28 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice } from '@reduxjs/toolkit';
 
 const initialState = {
-    cartIsVisible: false,
-}
+  cartIsVisible: false,
+  notification: null,
+};
 
 const uiSlice = createSlice({
-    name: 'ui',
-    initialState,
-    reducers: {
-        toggle: (state) => {
-            state.cartIsVisible = !state.cartIsVisible;
-        }
-    }
-})
+  name: 'ui',
+  initialState,
+  reducers: {
+    toggle: (state) => {
+      state.cartIsVisible = !state.cartIsVisible;
+    },
+    showNotification: (state, action) => {
+      state.notification = {
+        status: action.payload.status,
+        title: action.payload.title,
+        message: action.payload.message,
+      };
+    },
+  },
+});
 
-export const uiActions = uiSlice.actions
-export const  getCartStatus  = (state) => state.ui.cartIsVisible
+export const uiActions = uiSlice.actions;
+export const getCartStatus = (state) => state.ui.cartIsVisible;
 
 export default uiSlice;
